e68a141083961922ee3f1b884499eb8182cf5b93
oracle-dstrial2 <35608952+oracle-dstrial2@users.noreply.github.com>
2018-01-30 15:53:53 -0800

    first commit

---
+++ deploy-me.py
@@ -0,0 +1,17 @@
+###################
+# Any imports, loading of variables or models, and other preprocessing code would go here
+#
+#
+#
+#
+###################
+
+
+def deploy_function(variable_1, variable_2):
+         
+    # Here you would compute machine learning predictions, then return the results    
+        
+    return "Prediction based on inputs: "+str(variable_1)+", "+str(variable_2)
+
+
+
